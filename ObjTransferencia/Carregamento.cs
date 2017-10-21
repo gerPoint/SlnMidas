a84413d4522edc5b601755e1eb4dcb754b3f226e
satanice <diegoredip2k@gmail.com>
2017-10-21 15:54:51 -0200

    up project

---
+++ ObjTransferencia/Carregamento.cs
@@ -0,0 +1,58 @@
+﻿namespace ObjTransferencia
+{
+    class Carregamento
+    {
+        public int IDCarregamento { get; set; };
+        public int IDRomaneio { get; set; };
+        public float BlocoQtd1 { get; set; };
+        public string DescBlocoQtd1 { get; set; };
+        public float BlocoQtd2 { get; set; };
+        public string DescBlocoQtd2 { get; set; };
+        public float BlocoQtd3 { get; set; };
+        public string DescBlocoQtd3 { get; set; };
+        public float BlocoQtd4 { get; set; };
+        public string DescBlocoQtd4 { get; set; };
+        public float BlocoQtd5 { get; set; };
+        public string DescBlocoQtd5 { get; set; };
+        public float BlocoQtd6 { get; set; };
+        public string DescBlocoQtd6 { get; set; };
+        public float BlocoQtd7 { get; set; };
+        public string DescBlocoQtd7 { get; set; };
+        public float BlocoQtd8 { get; set; };
+        public string DescBlocoQtd8 { get; set; };
+        public float BlocoQtd9 { get; set; };
+        public string DescBlocoQtd9 { get; set; };
+        public float BlocoQtd10 { get; set; };
+        public string DescBlocoQtd10 { get; set; };
+        public float BlocoQtd11 { get; set; };
+        public string DescBlocoQtd11 { get; set; };
+        public float BlocoQtd12 { get; set; };
+        public string DescBlocoQtd12 { get; set; };
+        public float BlocoQtd13 { get; set; };
+        public string DescBlocoQtd13 { get; set; };
+        public float BlocoQtd14 { get; set; };
+        public string DescBlocoQtd14 { get; set; };
+        public float BlocoQtd15 { get; set; };
+        public string DescBlocoQtd15 { get; set; };
+        public float BlocoQtd16 { get; set; };
+        public string DescBlocoQtd16 { get; set; };
+        public float BlocoQtd17 { get; set; };
+        public string DescBlocoQtd17 { get; set; };
+        public float BlocoQtd18 { get; set; };
+        public string DescBlocoQtd18 { get; set; };
+        public float BlocoQtd19 { get; set; };
+        public string DescBlocoQtd19 { get; set; };
+        public float BlocoQtd20 { get; set; };
+        public string DescBlocoQtd20 { get; set; };
+        public float BlocoQtd21 { get; set; };
+        public string DescBlocoQtd21 { get; set; };
+        public float BlocoQtd22 { get; set; };
+        public string DescBlocoQtd22 { get; set; };
+        public float BlocoQtd23 { get; set; };
+        public string DescBlocoQtd23 { get; set; };
+        public float BlocoQtd24 { get; set; };
+        public string DescBlocoQtd24 { get; set; };
+        public float BlocoQtd25 { get; set; };
+        public string DescBlocoQtd25 { get; set; };
+    }
+}
