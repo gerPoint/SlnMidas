b0f3183702aa1cfa2379abbcc3d1e7979498e020
satanice <diegoredip2k@gmail.com>
2017-10-21 17:25:58 -0200

    alteração obj na classe de objetotransferencia

--- ObjTransferencia/obFuncionario.cs
+++ ObjTransferencia/obFuncionario.cs
@@ -2,16 +2,16 @@
 
 namespace ObjTransferencia
 {
-    class Usuario
+    class obFuncionario
     {
-        public int IDUsuario { get; set; }
+        public int IDFuncionario { get; set; }
         public string Nome { get; set; }
-        public string Senha { get; set; }
-        public string Cpf { get; set; }
         public string Rg { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
         public string Endereco { get; set; }
+        public char Status { get; set; }
         public DateTime DataCadastro { get; set; }
+
     }
 }
