85ea351d32cf162778fd770d1c6e24bd31923d3f
satanice <diegoredip2k@gmail.com>
2017-10-21 18:46:51 -0200

    tela de cadastrar cliente

--- Apresentacao/frmCliente.cs
+++ Apresentacao/frmCliente.cs
@@ -15,10 +15,9 @@ namespace Apresentacao
 {
     public partial class frmCliente : Form
     {
-        private Cliente cliente;
+        private objCliente cliente = new objCliente();
         string mensagem = "";
         NegCliente negCliente = new NegCliente();
-        Cliente Cliente = new Cliente();
         DateTime data_hora;
 
         public frmCliente()
@@ -43,11 +42,11 @@ namespace Apresentacao
             this.Close();
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            data_hora = DateTime.Now;
-            lbl_data_hora.Text = data_hora.ToString();
-        }
+        //private void timer1_Tick(object sender, EventArgs e)
+        //{
+        //    data_hora = DateTime.Now;
+        //    lbl_data_hora.Text = data_hora.ToString();
+        //}
 
        
 
@@ -56,15 +55,15 @@ namespace Apresentacao
             try
             {
 
-                this.Cliente.Nome = cpoNome.Text.TrimStart();
-                this.Cliente.RazaoSocial = cpoRazaoSocial.Text;
-                this.Cliente.Cnpj = cpoCnpj.Text.TrimStart();
-                this.Cliente.Endereco = cpoEndereco.Text.TrimStart();
-                this.Cliente.Cidade = cpoCidade.Text.TrimStart();
-                this.Cliente.Uf = cpoUf.Text.TrimStart();
-                this.Cliente.Telefone = cpoTelefone.Text.TrimStart();
-                this.Cliente.Email = cpoEmail.Text.TrimStart();
-                this.Cliente.Cpf = cpoCpf.Text.TrimStart();
+                this.cliente.Nome = cpoNome.Text.TrimStart();
+                this.cliente.RazaoSocial = cpoRazaoSocial.Text;
+                this.cliente.Cnpj = cpoCnpj.Text.TrimStart();
+                this.cliente.Endereco = cpoEndereco.Text.TrimStart();
+                this.cliente.Cidade = cpoCidade.Text.TrimStart();
+                this.cliente.Uf = cpoUf.Text.TrimStart();
+                this.cliente.Telefone = cpoTelefone.Text.TrimStart();
+                this.cliente.Email = cpoEmail.Text.TrimStart();
+                this.cliente.Cpf = cpoCpf.Text.TrimStart();
 
 
                 //   if (cpoAtivo.Checked == true) { objCliente.Ativo = "1"; } else { objCliente.Ativo = "0"; }
@@ -84,7 +83,18 @@ namespace Apresentacao
                     MessageBox.Show("Campo Obrigatório não informado.");
                     return;
                 }
-                strRetorno = this.negCliente.Cadastrar(this.Cliente);
+
+                strRetorno = this.negCliente.Cadastrar(this.cliente);
+
+                if (strRetorno != null)
+                {
+                    MessageBox.Show(strRetorno);
+                }
+                else
+                {
+                    MessageBox.Show("Está nulo");
+                }
+                   
 
 
                // strRetorno = negCliente.Cadastrar(this.objCliente);
